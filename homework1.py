af1af15ac2c9fa0fe909c8ab8b8d3b67e05bddfd
vladlvov <105988540+vladlvov@users.noreply.github.com>
2024-07-29 22:51:24 +0500

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,14 @@
+print("1st program")
+print(9 ** 0.5 * 5)
+print("2nd program")
+print(9.99 > 9.98 and 1000 != 1000.1)
+print("3rd program")
+print(1234 % 1000 // 10 + 5678 % 1000 // 10 )
+print("4th program")
+print(int(13.42 * 100 // 100))
+print(int(42.13 / 100 * 100))
+print(13.42 != 42.13 and 13 == 13)
+print(42.13 <= 13.42 or 42 == 42)
+
+
+
